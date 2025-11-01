d778f59e3328e32a447d898450cfc3532f341b74
Alyson_ <202320503@urp.edu.pe>
2025-11-01 18:46:55 -0500

    curtiñopolliño

--- sigeve-bd/src/main.jsx
+++ sigeve-bd/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Customers from './pages/customers.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     {/* <App /> */}
+     <Customers />
   </StrictMode>,
 )
