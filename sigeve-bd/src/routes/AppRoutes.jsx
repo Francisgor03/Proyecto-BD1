764fdebcb231fb437eab6cb412c2a2bcea4cccde
francisgor03 <francisco.d.curtocachique@gmail.com>
2025-11-09 15:31:34 -0500

    correcion

--- sigeve-bd/src/routes/AppRoutes.jsx
+++ sigeve-bd/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Customers from '../pages/Customers';
 import Orders from '../pages/Orders';
 import Products from '../pages/Products';
 import Suppliers from '../pages/Suppliers';
+import Categories from '../pages/Categories';
 
 
 export default function AppRoutes() {
@@ -17,6 +18,7 @@ export default function AppRoutes() {
           <Route path='/orders' element={<Orders />} />
           <Route path='/products' element={<Products />} />
           <Route path='/suppliers' element={<Suppliers />} />
+          <Route path='/categories' element={<Categories />} />
         </Route>
       </Routes>
     </BrowserRouter>
