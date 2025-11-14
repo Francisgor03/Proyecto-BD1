fade899a3289c410b9030d7ae7cefbde330a8529
francisgor03 <francisco.d.curtocachique@gmail.com>
2025-11-14 17:54:35 -0500

    iconos de mi profe fav <3

--- sigeve-bd/src/components/Sidebar.jsx
+++ sigeve-bd/src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
 import { Box, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import CategoryIcon from '@mui/icons-material/Category';
+import ArticleIcon from '@mui/icons-material/Article';
+import MapIcon from '@mui/icons-material/Map';
+import RoomIcon from '@mui/icons-material/Room';
+import WarehouseIcon from '@mui/icons-material/Warehouse';
 import { useNavigate } from 'react-router-dom';
 
 export default function Sidebar() {
@@ -9,13 +16,13 @@ export default function Sidebar() {
   const menu = [
   { text: 'Dashboard', icon: <DashboardIcon />, path: '/' },
   { text: 'Clientes', icon: <PeopleIcon />, path: '/customers' },
-  { text: 'Ordenes', icon: <PeopleIcon />, path: '/orders' },
-  { text: 'Productos', icon: <PeopleIcon />, path: '/products' },
-  { text: 'Proveedores', icon: <PeopleIcon />, path: '/suppliers' },
-  { text: 'Categorías', icon: <PeopleIcon />, path: '/categories' },
-  { text: 'Remitentes', icon: <PeopleIcon />, path: '/shippers' },
-  { text: 'Regiones', icon: <PeopleIcon />, path: '/region' },
-  { text: 'Territorios', icon: <PeopleIcon />, path: '/territories' }
+  { text: 'Ordenes', icon: <ArticleIcon />, path: '/orders' },
+  { text: 'Productos', icon: <Inventory2Icon />, path: '/products' },
+  { text: 'Proveedores', icon: <WarehouseIcon />, path: '/suppliers' },
+  { text: 'Categorías', icon: <CategoryIcon />, path: '/categories' },
+  { text: 'Remitentes', icon: <LocalShippingIcon />, path: '/shippers' },
+  { text: 'Regiones', icon: <MapIcon />, path: '/region' },
+  { text: 'Territorios', icon: <RoomIcon />, path: '/territories' }
 ];
 
 
